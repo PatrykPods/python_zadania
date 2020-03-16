30d6e0c4dcc4369ed92a6cab99baa57c11fcf246
patrykpods <patryk.podsiadlo@gmail.com>
2020-03-16 17:03:16 +0100

    zadanie 1.1

--- zad_1.1.py
+++ zad_1.1.py
@@ -1,13 +1,19 @@
-cena = float(input("podaj cene za kg. ziemniakow:"))
-ilosc = float(input("ile kg. ziemniakow chcesz kupic?:"))
-koszt = cena * ilosc
+cena_ziemniakow = float(input("podaj cene za kg. ziemniakow:"))
+ilosc_ziemniakow = float(input("ile kg. ziemniakow chcesz kupic?:"))
+koszt_ziemniakow = cena_ziemniakow * ilosc_ziemniakow
 
 ilosc_bananow = float(input("ile kg. bananow chcesz kupic?:"))
 cena_bananow = float(input("podaj cene za kg. bananow:"))
 koszt_bananow = ilosc_bananow * cena_bananow
 
 
-laczny_koszt = koszt_bananow + koszt
+laczny_koszt = koszt_bananow + koszt_ziemniakow
 
-print(f"laczny_koszt za {ilosc:.2f}kg ziemniakow oraz {ilosc_bananow:.2f}kg bananow wynosi {laczny_koszt:.2f}PLN")
+print(f"laczny_koszt za {ilosc_ziemniakow:.2f}kg ziemniakow oraz {ilosc_bananow:.2f}kg bananow wynosi {laczny_koszt:.2f}PLN")
 
+if koszt_bananow < koszt_ziemniakow:
+    print("za ziemniaki zaplacimy wiecej niz za banany")
+if koszt_bananow > koszt_ziemniakow:
+    print("za banany zaplacimy wiecej niz za ziemniaki")
+if koszt_bananow == koszt_ziemniakow:
+    print("banany kosztują tyle samo co ziemniaki")
