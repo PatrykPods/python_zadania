f222880b1a92a7e54a968c80e8be7f897f207911
patrykpods <patryk.podsiadlo@gmail.com>
2020-03-29 11:08:57 +0200

    zadanie 1.3

--- zadani_8.py
+++ zadani_8.py
@@ -3,6 +3,8 @@ b = float(input("podaj szerokosc opakowania:"))
 h = float(input("podaj wysokosc opakowania:"))
 
 objetosc = (a * b) * h
-print(f""" objetosc opakowania to: {objetosc} cm3
+
+print(f""" 
+objetosc opakowania to: {objetosc} cm3
 czy jest ona wieksza od 1 litra?: {objetosc >= 1000}
 """)
