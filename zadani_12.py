a0fd7398a3b1198cf226cfadde03dcbe5e1851d7
patrykpods <patryk.podsiadlo@gmail.com>
2020-03-29 13:56:42 +0200

    zadanie 12

--- zadani_12.py
+++ zadani_12.py
@@ -1,7 +1,7 @@
 x = 8
 temp = 0
 while x > 0:
-    temperature = float(input("podaj temperaturę w kolejnych dniach tygodnia zaczynając od poniedziałku:")
+    temperature = float(input("podaj temperaturę w kolejnych dniach tygodnia zaczynając od poniedziałku:"))
     t = temp + temperature
     x -= 1
 
